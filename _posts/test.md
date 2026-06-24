9acbd25f313081ecce7a63c2df7940b807e6d774
bodamiruSE <bodamiruse@gmail.com>
2026-06-24 22:28:53 +0200

    UNDER MAINTENANCE

--- _posts/test.md
+++
@@ -1,8 +0,0 @@
----
-title: test
-tag: Linux
-read_time: 5
-date: 2026-06-24
----
-
-test
--- script.js
+++
@@ -1,5 +0,0 @@
-// Hamburger Menu
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-}
